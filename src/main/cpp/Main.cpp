b47f9c070a6d6381b67f5b7c953dec4c31455ed5
Mr. Daehn <jdaehn@missouristate.edu>
2017-08-24 22:16:25 -0500

    CSC232-LEC01 - Initial import of main, demo, test project structure.

---
+++ src/main/cpp/Main.cpp
@@ -0,0 +1,19 @@
+/**
+ * CSC232 Data Structures with C++
+ * Missouri State University, Fall 2017
+ *
+ * @file Main.cpp
+ * @authors Jim Daehn <jdaehn@missouristate.edu>
+ *          Your Name <yname@missouristate.edu>
+ *          Partner Name <pname@missouristate.edu>
+ * @brief   Entry point for this application. This is nothing more than a playground.
+ */
+
+#include <cstdlib>
+#include <iostream>
+
+int main(int argc, char **argv) {
+    std::cout << "Hello, Main" << std::endl;
+
+    return EXIT_SUCCESS;
+}
