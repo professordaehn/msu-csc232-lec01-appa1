4a28dab083b3fb7c967722eba15433d79367c6a3
Mr. Daehn <jdaehn@missouristate.edu>
2017-08-25 13:11:11 -0500

    CSC232-LEC01 - Some initial refactoring.

--- src/main/cpp/Demo.cpp
+++ src/main/cpp/Demo.cpp
@@ -10,16 +10,26 @@
 #include <cstdlib>
 #include <iostream>
 
+double getRadius();
+
 int main(int argc, char **argv) {
     // Compute the volume of a sphere of a given radius
     const double PI = 3.14159;
-    double radius;
+    double radius{getRadius()};
 
-    std::cout << "Enter the radius of the sphere: ";
-    std::cin >> radius;
+//    std::cout << "Enter the radius of the sphere: ";
+//    std::cin >> radius;
+
 
     double volume = 4 * PI * radius * radius * radius / 3;
     std::cout << "The volume of a sphere of radius " << radius << " inches is " << volume << " cubic inches.\n";
 
     return EXIT_SUCCESS;
+}
+
+double getRadius() {
+    std::cout << "Enter the radius of the sphere: ";
+    double r;
+    std::cin >> r;
+    return r;
 }
